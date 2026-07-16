eef2335ef2ccf57b5a5984a50d79379805eb1030
Isaac <isaac.cloudtech@gmail.com>
2026-07-16 13:15:00 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,33 +1,15 @@
 provider "aws" {
   region = "ap-south-1"
 }
+
 data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "game_server" {
-  ami           = "ami-01a00762f46d584a1" 
-  instance_type = "t3.small"
-  key_name      = "ubuntu"
-
-  vpc_security_group_ids = [aws_security_group.jj.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt update -y
-              apt install nginx -y
-              systemctl start nginx
-              systemctl enable nginx
-              EOF
-
-  tags = {
-    Name = "Game-Server"
-  }
-}
-
 resource "aws_security_group" "games" {
-  name = "games"
- vpc_id = data.aws_vpc.default.id
+  name        = "games-v2"
+  description = "Managed by Terraform"
+  vpc_id      = data.aws_vpc.default.id
 
   ingress {
     from_port   = 22
@@ -49,7 +31,32 @@ resource "aws_security_group" "games" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "games-v2"
+  }
 }
+
+resource "aws_instance" "game_server" {
+  ami                    = "ami-01a00762f46d584a1"
+  instance_type          = "t3.small"
+  key_name               = "ubuntu"
+
+  vpc_security_group_ids = [aws_security_group.games.id]
+
+  user_data = <<-EOF
+#!/bin/bash
+apt update -y
+apt install nginx -y
+systemctl enable nginx
+systemctl start nginx
+EOF
+
+  tags = {
+    Name = "Game-Server"
+  }
+}
+
 output "public_ip" {
   value = aws_instance.game_server.public_ip
 }
